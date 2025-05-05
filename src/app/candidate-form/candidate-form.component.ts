a860bfdb8d91c4f49ca7f398a1429734f77c45ed
alubeda <gua903@gmail.com>
2025-05-05 18:56:48 +0200

    Add Excel file parsing and autofill for candidate data

--- src/app/candidate-form/candidate-form.component.ts
+++ src/app/candidate-form/candidate-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Workbook } from 'exceljs';
 
 @Component({
     selector: 'app-candidate-form',
@@ -14,6 +17,8 @@ import { MatButtonModule } from '@angular/material/button';
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
+        MatSelectModule,
+        MatCheckboxModule,
   ],
   templateUrl: './candidate-form.component.html',
   styleUrl: './candidate-form.component.less'
@@ -25,7 +30,39 @@ export class CandidateFormComponent {
     form = this.fb.group({
         name: ['', Validators.required],
         surname: ['', Validators.required],
+        seniority: ['', Validators.required],
+        yearsOfExperience: this.fb.control<number | null>(null, [Validators.required, Validators.min(0)]),
+        availability: [false, Validators.required],
     });
+
+    async onFileChange(event: any) {
+        const file = event.target.files[0];
+
+        if (file) {
+            return this.processFile(file);
+        }
+    }
+
+    private async processFile(file: File) {
+        const buffer = await file.arrayBuffer();
+        const workbook = new Workbook();
+        await workbook.xlsx.load(buffer);
+
+        const worksheet = workbook.worksheets[0];
+        const row = worksheet.getRow(1);
+
+        const seniority = row.getCell(1).text;
+        const yearsOfExperience = parseInt(row.getCell(2).value as string);
+        const availability = row.getCell(3).text === '1';
+
+        console.warn('DAME VALORES', { seniority, yearsOfExperience, availability });
+
+        this.form.patchValue({
+            seniority,
+            yearsOfExperience,
+            availability,
+        });
+    }
 
     submit() {
         if (this.form.valid) {
